761b44b8d39eb32b663ff43788dcf42a5e8ff0da
AndreiSarandan <80057863+AndreiSarandan@users.noreply.github.com>
2022-10-01 20:39:06 +0300

    Update bitcoin_price_index_cs50.py

--- bitcoin_price_cs50/bitcoin_price_index_cs50.py
+++ bitcoin_price_cs50/bitcoin_price_index_cs50.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import requests
 
 try:
@@ -8,3 +9,57 @@ try:
     print(z)
 except requests.RequestException:
     print("error")
+=======
+import random
+
+
+def main():
+    lvl = get_level()
+    generate_integer(lvl)
+
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Select level: "))
+            break
+        except ValueError:
+            pass
+    if level != 1 and level != 2 and level != 3:
+        get_level()
+    return level
+
+
+def generate_integer(level):
+    score = 10
+    if level == 1:
+        i1 = 0
+        i2 = 9
+    elif level == 2:
+        i1 = 10
+        i2 = 99
+    elif level == 3:
+        i1 = 100
+        i2 = 999
+
+    for i in range(10):
+        ok = 0
+        x = random.randint(i1, i2)
+        y = random.randint(i1, i2)
+        for i in range(3):
+            print(f'{x} + {y} =')
+            s = int(input())
+            if(s == x+y):
+                ok = 1
+                break
+            else:
+                print("EEE")
+        if ok == 0:
+            print(x+y)
+            score -= 1
+    print(score)
+
+
+if __name__ == "__main__":
+    main()
+>>>>>>> parent of 3c5aa4d (Rename bitcoin_price_index_cs50.py to Little Professor CS50)
